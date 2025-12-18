c65499e51ed7e633a7b2213db71b28b739fef0bd
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-12-18 10:23:05 -0600

    core: show tasks in project creation order instead of reverse chronological

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -18,7 +18,7 @@ class Project extends Model
 
     public function tasks()
     {
-        return $this->hasMany(Task::class)->latest();
+        return $this->hasMany(Task::class);
     }
 
     public function sections()
