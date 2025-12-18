087e39e8672fce3bfb58f1346d915f972ba8045f
oliverservin <7003648+oliverservin@users.noreply.github.com>
2025-12-18 00:14:00 +0000

    fix code style

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -46,7 +46,7 @@ class UserFactory extends Factory
                 ->state(array_merge([
                     'user_id' => $user->id,
                     'personal' => true,
-                    'name' => $user->name . '\'s Team',
+                    'name' => $user->name.'\'s Team',
                 ], $overrides))
                 ->create();
         });
